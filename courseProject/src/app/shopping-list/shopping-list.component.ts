52bae1feacdcb6072ae2d8cfbccb40bc50e4d160
Zeno França Junior <zfrancajunior@gmail.com>
2018-06-08 21:27:09 -0300

    update project

--- courseProject/src/app/shopping-list/shopping-list.component.ts
+++ courseProject/src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 15)
+  ];
+
   constructor() { }
 
   ngOnInit() {
